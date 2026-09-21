0b9d53b20afaa0984128304408f96156b8550f24
Jmeow <j@jmeow.net>
2026-09-21 16:28:35 -0500

    new stuff, coin movement, etc

--- coin.gd
+++ coin.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
+var coinState = 0
+#var coinState = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#$"../Player".position.x = 416
 	pass
 
 
@@ -12,4 +15,15 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	visible = false
+	if(coinState == 3):
+		coinState = 4
+		$"../Player".position.y += 272.0
+		visible = false
+	if(coinState == 2):
+		coinState = 3
+		visible = false
+	if(coinState == 1):
+		$"../Player".position.x += 384.0
+		coinState = 2
+		position.x = 920
+		position.y = 184
--- player.gd
+++ player.gd
@@ -2,16 +2,29 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
+const JUMP_VELOCITY = -176.0
 
 
 func _physics_process(delta: float) -> void:
-	if position.x > 200.0:
+	if position.x > 568.0 && $"../Coin".coinState < 3:
+		$"../ArrowHint".visible = $"../Coin".position.x > 440.0
 		position.x -= 16.0
-		if($"../Coin".position.x > 60.0):
+		if($"../Coin".position.x > 424.0):
 			$"../Coin".position.x -= 16.0
-	if position.y > 316.0:
+	if position.y > 316.0 && $"../Coin".coinState < 3:
 		position.y -= 400.0
+		if($"../Coin".coinState == 1):
+			$"../Coin".visible = true
+		if($"../Coin".coinState == 0):
+			$"../Coin".coinState = 1
+	if($"../Coin".coinState == 3 && position.y < 800.0 && position.x < 256.0):
+		position.y += 272.0
+	if($"../Coin".coinState == 3 && position.y > 800.0 && position.x > 400.0):
+		$"../Coin".visible = true
+		$"../Coin".position.x = 216
+		$"../Coin".position.y = 952
+	if(position.y > 1328.0 && position.x < 1000.0):
+		position.x += 656.0
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -23,6 +36,8 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+	if($"../Coin".coinState == 3 && position.y < 300.0 && direction > 0):
+		direction = 0
 	if direction:
 		velocity.x = direction * SPEED
 	else:
